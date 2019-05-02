e89566b8f869b4b197e32c5b03cbfad0ea58bd27
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2019-05-02 14:20:32 +0200

    just simple

--- ReactFrontEnd/src/index.tsx
+++ ReactFrontEnd/src/index.tsx
@@ -3,23 +3,23 @@ import * as ReactDOM from "react-dom";
 import ReactApplication from "./application";
 import { BrowserRouter as Router} from "react-router-dom";
 
-// function Render(props: { text: string }): JSX.Element {
-//     const [name, updateName] = React.useState(props.text);
-//     const [lazyComponent, loadComponent] = React.useState<JSX.Element>(<div>not loaded</div>);
+function Render(props: { text: string }): JSX.Element {
+    const [name, updateName] = React.useState(props.text);
+    const [lazyComponent, loadComponent] = React.useState<JSX.Element>(<div>not loaded</div>);
 
-//     return (
-//         <div>
-//             <button onClick={() => {
-//                 const NewComponent = React.lazy(() => import(/* webpackChunkName: "my-lazy" */"./lazyComponent"))
-//                 loadComponent(<NewComponent />);
-//             }}>Load</button>
-//             <input type="text" value={name} onChange={e => updateName(e.currentTarget.value)} />
-//             <React.Suspense fallback={() => <div>Loading ...</div>}>
-//                 {lazyComponent}
-//             </React.Suspense>
-//             </div>
-//     );
-// }
+    return (
+        <div>
+            <button onClick={() => {
+                const NewComponent = React.lazy(() => import(/* webpackChunkName: "my-lazy" */"./lazyComponent"))
+                loadComponent(<NewComponent />);
+            }}>Load</button>
+            <input type="text" value={name} onChange={e => updateName(e.currentTarget.value)} />
+            <React.Suspense fallback={() => <div>Loading ...</div>}>
+                {lazyComponent}
+            </React.Suspense>
+            </div>
+    );
+}
 
-ReactDOM.render(<Router><ReactApplication /></Router>, document.getElementById("container"));
-// ReactDOM.render(<div><Render text="default"></Render></div>, document.getElementById("container"));
+// ReactDOM.render(<Router><ReactApplication /></Router>, document.getElementById("container"));
+ReactDOM.render(<div><Render text="default"></Render></div>, document.getElementById("container"));
