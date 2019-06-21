88367ba4b9fe93f1a03f34e4716cc86a8ae3d4e9
Arbor <arbor@o2.pl>
2019-06-21 22:21:07 +0200

    cxzczx

--- ReactFrontEnd/src/components/login.button.tsx
+++ ReactFrontEnd/src/components/login.button.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
-import Button from '@material-ui/core/Button';
 import AuthorizeContext from '@bx-contexts/authorize.context';
 import { useSnackbar } from 'notistack';
 import AsyncButton from '@bx-components/async.button.component';
@@ -11,7 +10,6 @@ interface ILoginButtonProps {
 }
 
 export default function (props: ILoginButtonProps): JSX.Element {
-    const [loading, loadingChanged] = React.useState(false);
     const { t } = useTranslation();
     const { enqueueSnackbar } = useSnackbar();
 
@@ -22,24 +20,15 @@ export default function (props: ILoginButtonProps): JSX.Element {
                     value.isAuthorized
                         ? null
                         : <AsyncButton
-                            disabled={loading}
                             onClick={async () => {
-                                loadingChanged(true);
-                                try {
-                                    const { login } = await import('@bx-services/account');
-                                    await login();
-                                    props.onAuthorized();
-                                    enqueueSnackbar(t('hiUser'));
-                                    loadingChanged(false);
-                                } catch {
-                                    loadingChanged(false);
-                                }
+                                const { login } = await import('@bx-services/account');
+                                await login();
+                                props.onAuthorized();
+                                enqueueSnackbar(t('hiUser'));
                             }}
                         >
                             {
-                                loading
-                                    ? t('Loading')
-                                    : t(props.text || 'Login')
+                                t(props.text || 'Login')
                             }
                         </AsyncButton>
                 )
