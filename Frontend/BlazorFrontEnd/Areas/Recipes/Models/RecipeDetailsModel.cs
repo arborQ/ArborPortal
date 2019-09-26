5f16a5af5e4d01d4d42f8f5f98ddf38d07d72549
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2019-09-26 11:14:28 +0200

    dsa

---
+++ Frontend/BlazorFrontEnd/Areas/Recipes/Models/RecipeDetailsModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorFrontEnd.Areas.Recipes.Models {
+    public class RecipeDetailsModel {
+        public string RecipeId { get; set; }
+
+        public string RecipeName { get; set; }
+
+        public string RecipeDescription { get; set; }
+    }
+}
