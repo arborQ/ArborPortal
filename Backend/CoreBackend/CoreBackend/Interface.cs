97cc429937e927435dc3168feda0346bf4b45b0b
Lukasz Wojcik (PGS Software) <51819432+lwojcikpgs@users.noreply.github.com>
2020-01-31 14:09:08 +0100

    .net core 3.0

---
+++ Backend/CoreBackend/CoreBackend/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBackend
+{
+    interface Interface
+    {
+        void Log(Exception ex) => throw ex;
+    }
+}
